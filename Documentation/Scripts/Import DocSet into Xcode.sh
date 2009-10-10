a41690e773ff3be8e46d132d247bb27f30105ba8
André Berg <andre.bergmedia@googlemail.com>
2009-10-10 04:01:19 +0000

    Round 1: Moved mainly stuff belonging to the documentation around

---
+++ Documentation/Scripts/Import DocSet into Xcode.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+# Import DocSet into Xcode.sh
+# BMScriptTest
+#
+# Created by Andre Berg on 09.10.09.
+# Copyright 2009 Berg Media. All rights reserved.
+
+#  make will invoke docsetutil. Take a look at the Makefile to see how this is done.
+
+make -C "$SOURCE_ROOT/$DOCSET_NAME/html" install
+
+#  Construct a temporary applescript file to tell Xcode to load a docset.
+
+rm -f "$TEMP_DIR/loadDocSet.scpt"
+
+echo "tell application \"Xcode\"" >> "$TEMP_DIR/loadDocSet.scpt"
+echo "	load documentation set with path \"/Users/$USER/Library/Developer/Shared/Documentation/DocSets/\"" >> "$TEMP_DIR/loadDocSet.scpt"
+echo "end tell" >> "$TEMP_DIR/loadDocSet.scpt"
+
+# Run the load-docset applescript command. Use arch -i386 in front of it
+# to silence Scripting Addition x86_64 incompatibility errors.
+
+arch -i386 osascript "$TEMP_DIR/loadDocSet.scpt"
