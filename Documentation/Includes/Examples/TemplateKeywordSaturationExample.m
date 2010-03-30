b33ee3dcc442c41feb58852f946c02a3628d66e4
André Berg <andre.bergmedia@googlemail.com>
2010-03-31 01:43:49 +0200

    Add a keyword template example to the docs.

---
+++ Documentation/Includes/Examples/TemplateKeywordSaturationExample.m
@@ -0,0 +1,25 @@
+// Observe that even though you could just pass nil for the options
+// (yielding the same effect) it is usually better to supply the 
+// options as you will make your intent clear.
+
+BMScript * script3 = [[BMScript alloc] initWithTemplateSource:@"This is a %{KEYWORD} template. %{ADJECTIVE} stuff." 
+                                                      options:BMSynthesizeOptions(@"/bin/echo", @"-n")];
+
+// Before we can execute the script we need to saturate it first, 
+// e.g. fill it with values for the keywords. Otherwise we'd get
+// an exception which would tell us that we need to saturate the
+// template before execution.
+
+NSDictionary * keywordDict = [NSDictionary dictionaryWithObjectsAndKeys:@"keyword-based", @"KEYWORD", 
+                                                                        @"Neat", @"ADJECTIVE", nil];
+[script3 saturateTemplateWithDictionary:keywordDict];
+
+
+// Now that we have saturated the template we can execute it:
+
+NSInteger retVal = [script3 executeAndReturnResult:&result2];
+
+NSLog(@"script3 result = %@", result2);
+NSLog(@"script3 retVal = %d", retVal);
+// script3 result = This is a keyword-based template. Neat stuff.
+// script3 retVal = 0
