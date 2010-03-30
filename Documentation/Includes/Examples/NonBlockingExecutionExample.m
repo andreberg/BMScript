5f0c5fda0424d4bdad82074578460ad442020945
André Berg <andre.bergmedia@googlemail.com>
2010-03-31 01:41:10 +0200

    Rename NotificationExample.m to NonBlockingExecutionExample.m so it better fits with the terminology set earlier.

--- Documentation/Includes/Examples/NonBlockingExecutionExample.m
+++ Documentation/Includes/Examples/NonBlockingExecutionExample.m
@@ -12,12 +12,13 @@
 // Meanwhile, in your class...
 
 // Your selector will be called once the task has finished. 
-// You can query status and results through the NSNotification userInfo dictionary.
+// You can query status (return value, aka exit code) and results through 
+// the NSNotification userInfo dictionary.
 - (void) taskFinished:(NSNotification *)aNotification {
     
-    NSDictionary * infoDict = [aNotification userInfo];
+    NSDictionary * infoDict  = [aNotification userInfo];
     TerminationStatus status = [[infoDict objectForKey:BMScriptNotificationTaskTerminationStatus] intValue];
-    NSString * results = [infoDict objectForKey:BMScriptNotificationTaskResults];
+    NSString * results       = [infoDict objectForKey:BMScriptNotificationTaskResults];
     
     NSLog(@"Inside %s: background task finished with status = %ld, result = '%@'", 
             __PRETTY_FUNCTION__, status, [results quote]);
@@ -25,9 +26,13 @@
 
 // ...
 
-// DON'T FORGET to remove yourself as observer, ideally right when you're done with BMScript
-// but at least in your class' dealloc method or your program might crash if the defaultCenter 
-// continues to send messages to your dealloc'd class!
+// If you're not using Garbage Collection, DON'T FORGET to remove yourself as observer, ideally 
+// right when you're done with BMScript, but at least in your class' dealloc method or your program 
+// may crash if the defaultCenter continues to send messages to your dealloc'd class!
+
+// With GC enabled you DO NOT need to remove your class from the defaultCenter since the defaultCenter
+// under GC stores connections to your class as "Zeroing Weak References". You can just forget about
+// -dealloc or -finalize with regards to removing yourself as observer.
 - (void) dealloc {
     [[NSNotificationCenter defaultCenter] removeObserver:self];
 }
